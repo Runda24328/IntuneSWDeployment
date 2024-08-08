b8a0960037c2150f9873877e19c2257249794488
Daniel Mikš <107947257+Runda24328@users.noreply.github.com>
2024-08-08 15:49:16 +0200

    Update Set-CustomWallpaper_PlatformScript.ps1

--- SetWallpaper/Set-CustomWallpaper_PlatformScript.ps1
+++ SetWallpaper/Set-CustomWallpaper_PlatformScript.ps1
@@ -1,7 +1,7 @@
 $BackgroundImageURL = 'https://YourWebsite.example/Wallpaper.jpg'
 Start-BitsTransfer -Source $BackgroundImageURL -Destination C:\ProgramData\Wallpaper.jpg
 
-
+#Take ownership of default wallpapers so the SYSTEM can manipulate with them
 #Light theme
 & takeown /f C:\Windows\Web\Wallpaper\Windows\img0.jpg
 & icacls C:\Windows\Web\Wallpaper\Windows\img0.jpg /Grant 'System:(F)'
@@ -9,6 +9,7 @@ Start-BitsTransfer -Source $BackgroundImageURL -Destination C:\ProgramData\Wallp
 & takeown /f C:\Windows\Web\Wallpaper\Windows\img19.jpg
 & icacls C:\Windows\Web\Wallpaper\Windows\img19.jpg /Grant 'System:(F)'
 
+#Replace default wallpaper files with downloaded ones
 #Light theme
 Remove-Item C:\Windows\Web\Wallpaper\Windows\img0.jpg -Force
 Copy-Item -Path "C:\ProgramData\Wallpaper.jpg" -Destination "C:\Windows\Web\Wallpaper\Windows\img0.jpg" -Force
