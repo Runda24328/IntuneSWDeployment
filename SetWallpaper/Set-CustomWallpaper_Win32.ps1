06e3ba36a68ac72f805b7e839ea18ddc82a225c1
Daniel Mikš <107947257+Runda24328@users.noreply.github.com>
2024-08-08 15:42:03 +0200

    Create Set-CustomWallpaper_Win32.ps1

---
+++ SetWallpaper/Set-CustomWallpaper_Win32.ps1
@@ -0,0 +1,15 @@
+#Take over default Windows wallpapers
+
+#Light theme
+& takeown /f C:\Windows\Web\Wallpaper\Windows\img0.jpg
+& icacls C:\Windows\Web\Wallpaper\Windows\img0.jpg /Grant 'System:(F)'
+#Dark theme
+& takeown /f C:\Windows\Web\Wallpaper\Windows\img19.jpg
+& icacls C:\Windows\Web\Wallpaper\Windows\img19.jpg /Grant 'System:(F)'
+
+#Light theme
+Remove-Item C:\Windows\Web\Wallpaper\Windows\img0.jpg -Force
+Copy-Item $PSScriptRoot\<YourLightThemeImage.png> C:\Windows\Web\Wallpaper\Windows\img0.jpg
+#Dark theme
+Remove-Item C:\Windows\Web\Wallpaper\Windows\img19.jpg -Force
+Copy-Item $PSScriptRoot\<YourDarkThemeImage.png> C:\Windows\Web\Wallpaper\Windows\img19.jpg
